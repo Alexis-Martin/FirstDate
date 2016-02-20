a43987ce234735273eca183cc1087c96c987e3f8
Alexis-Martin <martinalexis90@gmail.com>
2016-02-20 21:00:07 +0100

    Put class Connection in a new file

---
+++ Connection.php
@@ -0,0 +1,80 @@
+<?php
+
+class Connection{
+
+      private $bdd;
+      private $_host;
+      private $_dbname;
+      private $_mdp;
+      private $_user;
+
+  public function __construct($h,$db,$user,$mdp){
+    $this->sethost($h);
+    $this->setdbname($db);
+    $this->setuser($user);
+    $this->setmdp($mdp);
+    try{
+        $this->bdd = new PDO('mysql:host='.$h.';dbname='.$db, $user, $mdp);
+    }
+    catch (Exception $e)
+    {
+        die('Erreur : ' . $e->getMessage());
+    }
+
+  }
+  public function getBDD(){
+    return $this->bdd;
+  }
+  public function sethost($h){
+    $this->_host = $h;
+  }
+  public function setdbname($db){
+    $this->_dbname = $db;
+  }
+  public function setuser($user){
+    $this->_user = $user;
+  }
+  public function setmdp($mdp){
+    $this->_mdp = $mdp;
+  }
+  public function gethost(){
+    return $this->$_host;
+  }
+  public function getdbname(){
+    return $this->$_dbname;
+  }
+  public function getuser(){
+    return $this->$_user;
+  }
+  public function getmdp(){
+    return $this->$_mdp;
+  }
+  public function query($req){
+    $res= $this->getBDD()->query($req);//'SELECT COUNT(*) as nb FROM likes');
+    //$res= $this->getBDD()->prepare($req);
+    //$res->execute();
+        if ($res!=NULL){
+            $rc=$res->rowCount();
+            return $res;
+        }
+        else{
+            return NULL;
+        }
+
+    while ($data = $requete->fetch(PDO::FETCH_ASSOC)) {
+            print_r ($data);
+    }
+
+   }
+   public function count($res){
+
+        if($res==NULL){
+            return 0;
+        }
+        else{
+            return $res->rowCount();
+        }
+    }
+}
+
+?>
